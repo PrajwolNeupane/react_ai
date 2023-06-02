abe0b93f4e399b7d897f8b8d80351f38532d2368
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-06-02 22:25:40 +0545

    Added certain actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,99 @@
 import "./App.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
+
   const [ourText, setOurText] = useState("")
   const msg = new SpeechSynthesisUtterance()
+  const [allVoices, setVoices] = useState([]);
+  const [currentVoice, setCurrentVoice] = useState(null);
+
+
+  const stringContainsArray = (str, array) => {
+    return array.some((element) => str.includes(element));
+  };
+  
 
   const speechHandler = (msg) => {
-    msg.text = ourText
-    window.speechSynthesis.speak(msg)
+    if(ourText.toLocaleLowerCase() == "who made you"){
+      msg.text = "Prajwol Neupane made me."
+      msg.voice = currentVoice ? currentVoice : allVoices[4];
+      msg.pitch = 1.1
+      msg.rate = 1
+      window.speechSynthesis.speak(msg)
+    }else if(ourText.toLocaleLowerCase() == 'play aparibhasit song'){
+      msg.text = "Playing aparibhasit."
+      msg.voice = currentVoice ? currentVoice : allVoices[4];
+      msg.pitch = 1.1
+      msg.rate = 1
+      window.speechSynthesis.speak(msg)
+      window.open("https://www.youtube.com/watch?v=Sc1OI1i-Kgs", '_blank');
+    }
+    else if(stringContainsArray(ourText.toLocaleLowerCase(),['hi','hello','holla'])){
+      msg.text = "Hello Prajwol, How are you doing. Need a help. I am here to help you."
+      msg.voice = currentVoice ? currentVoice : allVoices[4];
+      msg.pitch = 1.1
+      msg.rate = 1
+      window.speechSynthesis.speak(msg)
+    }
+    else{
+      msg.text = "Sorry. This AI is still on development process."
+      msg.voice = currentVoice ? currentVoice : allVoices[4];
+      msg.pitch = 1.1
+      msg.rate = 1
+      window.speechSynthesis.speak(msg)
+    }
+    
+
   }
+
+  const getVoice = () => {
+    setVoices(window.speechSynthesis.getVoices());
+    console.log(allVoices)
+  }
+
+  const setVoice = (voice) => {
+    setCurrentVoice(voice);
+  }
+
+  useEffect(() => {
+
+    getVoice();
+  }, []);
+
+  useEffect(() => {
+    const loaded = window.speechSynthesis.addEventListener('voiceschanged', () => {
+      getVoice();
+    })
+    return () => {
+      if (loaded) {
+        window.speechSynthesis.removeEventListener('voiceschanged', handleSpeechSynthesisReady);
+      }
+    }
+  });
+
+
+
 
   return (
     <div className='App'>
       <h1>React Text to Speech App</h1>
-      <input
+      <textarea
+      style={{width:"200px",height:"300px"}}
         type='text'
         value={ourText}
         placeholder='Enter Text'
-        onChange={(e) => setOurText(e.target.value)}
-      />
+        onChange={(e) => setOurText(e.target.value)} />
       <button onClick={() => speechHandler(msg)}>SPEAK</button>
+      {
+        allVoices.map((curr, indx) => {
+          return (
+            <p onClick={() => {
+              setVoice(curr)
+            }}>{curr?.name}</p>
+          );
+        })
+      }
     </div>
   )
 }
